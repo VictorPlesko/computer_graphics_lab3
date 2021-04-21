df61c15d78e9382d64ecb6d30a9850e8a2720e36
VictorPlesko <victorplesko@gmail.com>
2021-04-21 15:40:13 +0300

    Added Lab3

---
+++ ImageProcessing/ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitContainer1.Enabled = false;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.SplitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1067, 389);
+            this.splitContainer1.SplitterDistance = 533;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(529, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(525, 385);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(540, 419);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(523, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TabStop = false;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(553, 501);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select a structural element size:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 470);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Dilatation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 506);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Erosia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 470);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 28);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Disconnect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 506);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 28);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Closing";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(822, 500);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "3x3";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.CheckBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(887, 500);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 24);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "4x4";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.CheckBox2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox3.Location = new System.Drawing.Point(952, 500);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 24);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "5x5";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.CheckBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(178, 435);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Tools:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Lab3_Plesko";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImageProcessing/ImageProcessing/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        struct Pixel
+        {
+            public float colorGray;
+            public int x;
+            public int y;
+        }
+
+        private List<Bitmap> bitmaps = new List<Bitmap>(101);
+        private Bitmap processingBitmap;
+        private bool isBlocked = false;
+        private CheckBox lastChecked;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Image Processing";
+
+            checkBox1.Checked = true;
+        }
+
+        private void SplitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private async void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Text = "Processing... 0%";
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                for(int i = 0; i < bitmaps.Count; i++)
+                {
+                    bitmaps[i].Dispose();
+                }
+                bitmaps.Clear();
+
+                var bitmap = new Bitmap(openFileDialog1.FileName);
+                await Task.Run(() => { RunProcessing(bitmap); });
+                pictureBox2.Image = bitmaps[trackBar1.Value];
+            }
+        }
+
+        private void Expansion(Bitmap bitmap)
+        {
+            this.Invoke(new Action(() =>
+            {
+                Text = "Processing...";
+            }));
+
+            int step;
+            if (checkBox1.Checked)
+                step = 1;
+            else if (checkBox2.Checked)
+                step = 2;
+            else
+                step = 3;
+
+            if (processingBitmap != null)
+                processingBitmap.Dispose();
+
+            processingBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+            for (int y = step; y < bitmap.Height - step; y++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    Text = "Processing... " + (100 * y) / bitmap.Height + "%";
+                }));
+
+                for (int x = step; x < bitmap.Width - step; x++)
+                {
+                    Color color = bitmap.GetPixel(x, y);       
+                    if (color.R == 0 && color.G == 0 && color.B == 0)
+                    {
+                        
+                        for (int i = y-step; i <= y+step; i++)
+                        {
+                            for(int j = x-step; j <= x+step; j++)
+                            {
+                                processingBitmap.SetPixel(j, i, Color.Black);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        processingBitmap.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+
+            this.Invoke(new Action(() =>
+            {
+                Text = "Processing complite!";
+            }));
+            pictureBox2.Image = processingBitmap;
+        }
+
+        private void Eroisa(Bitmap bitmap)
+        {
+            this.Invoke(new Action(() =>
+            {
+                Text = "Processing...";
+            }));
+
+            int step;
+            if (checkBox1.Checked)
+                step = 1;
+            else if (checkBox2.Checked)
+                step = 2;
+            else
+                step = 3;
+
+            if (processingBitmap != null)
+                processingBitmap.Dispose();
+
+            processingBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+            for (int y = step; y < bitmap.Height - step; y++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    Text = "Processing... " + (100*y)/bitmap.Height + "%";
+                }));
+                for (int x = step; x < bitmap.Width - step; x++)
+                {
+                    processingBitmap.SetPixel(x, y, Color.White);
+                    bool setToBlack = true;
+                    for (int i = y - step; i <= y + step; i++)
+                    {
+                        setToBlack = true;
+                        for (int j = x - step; j <= x + step; j++)
+                        {
+                            Color color = bitmap.GetPixel(j, i);
+                            if (color.R == 255 && color.G == 255 && color.B == 255)
+                            {
+                                setToBlack = false;
+                                break;
+                            }
+                        }
+
+                        if (!setToBlack)
+                        {
+                            break;
+                        }
+                    }
+                    if (setToBlack)
+                    {
+                        processingBitmap.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            this.Invoke(new Action(() =>
+            {
+                Text = "Processing complite!";
+            }));
+            pictureBox2.Image = processingBitmap;
+        }
+
+        private void RunProcessing(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>(bitmap.Width * bitmap.Height);
+
+            for(int y = 0; y < bitmap.Height; y++)
+            {
+                for(int x = 0; x < bitmap.Width; x++)
+                {
+                    Color color = bitmap.GetPixel(x, y);
+                    float gray = (color.R * 0.3f + color.G * 0.59f + color.B * 0.11f);
+                    pixels.Add(new Pixel
+                    {
+                        colorGray = gray,
+                        x = x,
+                        y = y
+                    });
+                }
+            }
+
+            pixels.Sort((x, y) => x.colorGray.CompareTo(y.colorGray));
+            int pixelsPerIter = pixels.Count / 99;
+            Color black = Color.Black;
+            Color white = Color.White;
+
+            var curBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for(int i = 0; i < pixels.Count; i++)
+            {
+                curBitmap.SetPixel(pixels[i].x, pixels[i].y, white);
+            }
+            bitmaps.Add(new Bitmap(curBitmap));
+
+            int globalCounter = 1;
+            for(int i = 1; i < 100; i++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    Text = "Processing... " + i + "%";
+                }));
+
+                for (int j = 0; j < pixelsPerIter; j++)
+                {
+                    curBitmap.SetPixel(pixels[globalCounter].x, pixels[globalCounter].y, black);
+                    globalCounter++;
+                }
+                bitmaps.Add(new Bitmap(curBitmap));
+            }
+
+            for (int i = globalCounter; i < pixels.Count; i++)
+            {
+                curBitmap.SetPixel(pixels[i].x, pixels[i].y, black);
+            }
+            bitmaps.Add(new Bitmap(curBitmap));
+            curBitmap.Dispose();
+
+            this.Invoke(new Action(() =>
+            {
+                Text = "Processing complite!";
+            }));      
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(bitmaps.Count > 0)
+            {
+                pictureBox2.Image = bitmaps[trackBar1.Value];
+            }
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+
+            if (!isBlocked)
+            {
+                isBlocked = true;
+                await Task.Run(() =>
+                {
+                    Expansion(new Bitmap(pictureBox2.Image));
+                });
+                isBlocked = false;
+            }
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+
+            if (!isBlocked)
+            {
+                isBlocked = true;
+                await Task.Run(() =>
+                {
+                    Eroisa(new Bitmap(pictureBox2.Image));
+                });
+                isBlocked = false;
+            }
+        }
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+
+            if (!isBlocked)
+            {
+                isBlocked = true;
+                await Task.Run(() =>
+                {
+                    Eroisa(new Bitmap((Image)pictureBox2.Image.Clone()));
+                });
+
+                await Task.Run(() =>
+                {
+                    Expansion(new Bitmap((Image)pictureBox2.Image.Clone()));
+                });
+                isBlocked = false;
+            }
+        }
+
+        private async void Button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+                return;
+
+            if (!isBlocked)
+            {
+                isBlocked = true;
+                await Task.Run(() =>
+                {
+                    Expansion(new Bitmap((Image)pictureBox2.Image.Clone()));
+                });
+
+                await Task.Run(() =>
+                {
+                    Eroisa(new Bitmap((Image)pictureBox2.Image.Clone()));
+                });
+                isBlocked = false;
+            }
+        }
+
+        private void CheckBox1_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                checkBox1.Checked = true;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void CheckBox2_Click(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked == true)
+            {
+                checkBox2.Checked = true;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void CheckBox3_Click(object sender, EventArgs e)
+        {
+            if(checkBox3.Checked == true)
+            {
+                checkBox3.Checked = true;
+                checkBox2.Checked = false;
+                checkBox1.Checked = false;
+            }
+        }
+    }
+}
